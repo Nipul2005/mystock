bbff17a1db2222c3f5de21cd1b94d4f3eb4bb7ce
Nipul <pnipul7678@gmail.com>
2026-04-21 18:02:21 +0530

    FROM TODAY

--- src/landing/Dashboard.jsx
+++ src/landing/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function Dashboard() {
-  return <div className="border-2 border-primary col-span-10"></div>;
+  return <div className="border-2 border-primary sm:col-span-10 col-span-12">dashboard</div>;
 }
 
 export default Dashboard
--- src/landing/Sidebar.jsx
+++ src/landing/Sidebar.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Sidebar() {
   return (
-    <div className='border-2 border-primary col-span-2'></div>
+    <div className='border-2 border-primary col-span-0 sm:col-span-2 sm:flex hidden'>
+      
+    </div>
   )
 }
 
