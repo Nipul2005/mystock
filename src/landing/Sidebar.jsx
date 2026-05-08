41e7e364b728356b967b2d8d6c9aa29f9ae3e02e
Nipul Pandit <pnipul7678@gmail.com>
2026-05-08 23:11:59 +0530

    little bit

--- src/landing/Sidebar.jsx
+++ src/landing/Sidebar.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function Sidebar() {
   return (
-    <div className='border-2 border-primary col-span-0 sm:col-span-2 sm:flex hidden'>
-      
+    <div className="border-2 border-primary col-span-0 sm:col-span-2 sm:flex hidden flex-col">
+      <div className="w-full h-auto flex justify-center items-center border">
+        <h1 className="lg:text-2xl md:text-xl text-lg font-bold ">OUTless</h1>  
+      </div>
+      <div className="w-full border"> </div>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
