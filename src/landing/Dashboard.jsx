50242a0f4e38eb5357f147921631a5a1f769b713
Nipul Pandit <pnipul7678@gmail.com>
2026-05-23 11:43:52 +0530

    started dashoard

--- src/landing/Dashboard.jsx
+++ src/landing/Dashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Dashboard() {
   return (
     <div className="w-full h-full overflow-hidden lg:col-span-10 md:col-span-11 col-span-full md:row-span-12 row-span-11">
-      
+      <div className="w-full"></div>
     </div>
   );
 }
