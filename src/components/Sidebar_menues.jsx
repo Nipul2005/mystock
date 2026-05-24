b606f36162e828973fba3132c72189627cbd45e3
Nipul Pandit <pnipul7678@gmail.com>
2026-05-25 00:10:55 +0530

    chnages in header

--- src/components/Sidebar_menues.jsx
+++ src/components/Sidebar_menues.jsx
@@ -1,19 +1,16 @@
-function Sidebar_menues({name, icon_name, classes}) {
-
-    
+function Sidebar_menues({ name, icon_name, classes }) {
   return (
- <span
-          href="/explore"
-          className={`md:w-full w-auto flex lg:justify-start justify-center items-center gap-3 md:px-3 md:py-2 md:my-1 cursor-pointer md:hover:bg-primary md:hover:text-white rounded-lg shrink-0 ${classes}`}
-        >
-          <i
-            className={`ri-${icon_name}-line text-2xl rounded-full`}//ri-search-line text-2xl rounded-full
-            title={`${name}`}// icon adjacent name
-          ></i>
-          <h2 className="font-semibold lg:flex hidden">{name}</h2>
-        </span>
- 
-  )
+    <span
+      href="/explore"
+      className={`md:w-full w-auto flex lg:justify-start justify-center items-center gap-3 md:px-3 md:py-2 md:my-1 cursor-pointer md:hover:bg-primary md:hover:text-white rounded-lg shrink-0 transition-all duration-200 ease-in-out ${classes} `}
+    >
+      <i
+        className={`ri-${icon_name}-line text-2xl rounded-full`} //ri-search-line text-2xl rounded-full
+        title={`${name}`} // icon adjacent name
+      ></i>
+      <h2 className="font-semibold lg:flex hidden">{name}</h2>
+    </span>
+  );
 }
 
-export default Sidebar_menues
+export default Sidebar_menues;
--- src/landing/Dashboard.jsx
+++ src/landing/Dashboard.jsx
@@ -1,23 +1,42 @@
-import React from 'react'
-import Sidebar_menues from '../components/Sidebar_menues';
+import React from "react";
+import Sidebar_menues from "../components/Sidebar_menues";
 
 function Dashboard() {
   return (
     <div className="w-full h-full overflow-hidden lg:col-span-10 md:col-span-11 col-span-full md:row-span-12 row-span-11 px-5 py-5 space-y-10">
       <header className="w-full flex justify-between items-center gap-4">
-        <div>
-        <h1 className='lg:text-5xl md:text-4xl text-3xl text-accent font-bold'>Dashboard</h1>
-        <p>Manage your store here, with <span className='text-primary font-bold'>outless</span></p>
+        <div className="md:flex flex-col gap-2 flex-3 hidden">
+          <h1 className="lg:text-5xl md:text-4xl text-3xl text-accent font-bold">
+            Dashboard
+          </h1>
+          <p>
+            Manage your inventory, orders, and sales with{" "}
+            <span className="text-primary font-bold">outless</span>
+          </p>
         </div>
-      
-        <button className='px-4 border-primary rounded-full border-2 py-2 hover:bg-primary hover:text-white shadow-2xl text-accent cursor-pointer duration-300 md:flex hidden'>Create Product</button>
-        <Sidebar_menues name={"Setting"} icon_name={"equalizer"} classes={"md:hidden flex"}/>
+
+        <div className="border border-border py-1 pl-4 pr-2 flex-2 rounded-full flex justify-between items-center">
+          <input
+            name="search"
+            type="text"
+            className="w-full outline-none "
+            placeholder="Search Products..."
+          />
+
+          <i className="ri-search-line text-white border-black px-3 py-1 bg-primary rounded-full cursor-pointer"></i>
+        </div>
+
+        <Sidebar_menues
+          name={"Setting"}
+          icon_name={"equalizer"}
+          classes={"md:hidden flex"}
+        />
       </header>
 
       <div className="flex gap-2">
         <div className="flex-1 flex justify-between items-center px-4 py-3 text-3xl font-semibold text-accent bg-primary/20 rounded-lg">
-          <h3 className=''>Total stock</h3>
-          <p className=''>1</p>
+          <h3 className="">Total stock</h3>
+          <p className="">1</p>
         </div>
         <div className="flex-1 border">
           <h3>Total stock</h3>
@@ -30,4 +49,4 @@ function Dashboard() {
   );
 }
 
-export default Dashboard 
+export default Dashboard;
