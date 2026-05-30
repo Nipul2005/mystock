0e2f35c5fa648fdc3e5a825db9e57a167aa522fa
Nipul Pandit <pnipul7678@gmail.com>
2026-05-31 01:00:13 +0530

    little one

--- src/Dashbaord_content/InventoryShowPeace.jsx
+++ src/Dashbaord_content/InventoryShowPeace.jsx
@@ -2,16 +2,23 @@ import React from 'react'
 
 function InventoryShowPeace() {
   return (
-    <div className='w-full sm:py-16 sm:px-10 rounded-lg shadow-lg bg-white text-text-primary'>
+    <div className="w-full sm:py-16 sm:px-10 py-6 px-5 rounded-lg shadow-lg text-text-primary bg-white">
       <div className="flex justify-between items-center">
-        <span className='space-y-2'>
-          <h3 className='font-semibold text-5xl'>Inventory</h3>
-          <p className='text-sm text-text-muted'>Track your product and stock levels</p>
+        <span className="space-y-1">
+          <h3 className="lg:text-5xl md:text-4xl text-3xl  font-semibold">
+            Inventory
+          </h3>
+          <p className="text-sm text-text-muted">
+            Track your product and stock levels
+          </p>
         </span>
-        <button className='px-6 py-2 bg-accent rounded-full border-border shadow-lg text-white'>Add Product</button>
+        <button className="px-6 py-2 bg-accent rounded-full border-border shadow-lg text-white md:flex hidden">
+          Add Product
+        </button>
       </div>
+      <div className=""></div>
     </div>
-  )
+  );
 }
 
 export default InventoryShowPeace
