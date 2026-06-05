51e124cdf8c2cef050d6864bb02004ef14446fda
Nipul Pandit <pnipul7678@gmail.com>
2026-06-06 02:28:11 +0530

    added prod-dev differnvce

--- src/utils/BizSphere.js
+++ src/utils/BizSphere.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 const BizSphere = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.VITE_ENV === "Dev"
+      ? import.meta.env.VITE_DEV_URL
+      : import.meta.env.VITE_PROD_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
